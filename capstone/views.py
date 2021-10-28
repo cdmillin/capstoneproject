53548039174528142d073975bd2ab0b50b5d4863
willb <wbarrera@uwm.edu>
2021-10-28 18:13:22 -0500

    Added html files for front-end, modified views.py to work with front-end, added to urls, also added django and python interpreter to project to test out front end

--- capstone/views.py
+++ capstone/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
 # Create your views here.
+class Home(View):
+    def get(self, request):
+        return render(request, "home.html")
+
+    def post(self, request):
+        pass
+
+class About(View):
+    def get(self, request):
+        return render(request, 'about.html')
--- capstone2/urls.py
+++ capstone2/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from capstone.views import Home, About
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home.as_view()),
+    path('about/', About.as_view())
 ]
