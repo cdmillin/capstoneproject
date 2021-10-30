4427da3edaa32662de9c963f0f75e4cf5e415018
bendemuri <bpdemuri@uwm.edu>
2021-10-30 14:00:45 -0500

    Worked on home page post method to tie template to API

--- capstone/api_translation.py
+++ capstone/api_translation.py
@@ -1,27 +1,33 @@
 import requests
-
+from requests.auth import HTTPBasicAuth
+auth = {'X-Triposo-Account': 'S5H29H39', 'X-Triposo-Token': 'hg4j9s8dqqu0spgwe32c7h71xykwzngq'} # hg4j9s8dqqu0spgwe32c7h71xykwzngq = Ben_test api token
 
 class Api_Translation:
 
     def top_cities_in_country(self, location):
-        awaiting_submission = "https://www.triposo.com/api/20211011/location.json?part_of=" + location + "&tag_labels=city&count=10&fields=id,name,score,snippet&order_by=-score"
-        response = requests.get(awaiting_submission)
+        awaiting_submission = "https://www.triposo.com/api/20211011/location.json?part_of=" + location + "&tag_labels=city&count=2&fields=id,name,score,snippet&order_by=-score"
+        # Using headers=auth when calling request sends authentication to avoid 401 error code
+        response = requests.get(awaiting_submission, headers=auth)
         cities = []
 
         if response.status_code != 200:
+            print("Error requesting data from Triposo API: ", response.status_code)
             return cities
-        restaurant_data = response.json()['results']
-        for x in restaurant_data:
+
+        city_data = response.json()['results']
+        for x in city_data:
             cities.append(x['id'])
 
         return cities
 
     def find_best_resturants(self, location):
-        awaiting_submission = "https://www.triposo.com/api/20211011/poi.json?location_id=" + location + "&tag_labels=eatingout&count=10&fields=id,name,score,intro,tag_labels,best_for&order_by=-score"
-        response = requests.get(awaiting_submission)
+        awaiting_submission = "https://www.triposo.com/api/20211011/poi.json?location_id=" + location + "&tag_labels=eatingout&count=2&fields=id,name,score,intro,tag_labels,best_for&order_by=-score"
+        # Using headers=auth when calling request sends authentication to avoid 401 error code
+        response = requests.get(awaiting_submission, headers=auth)
         restaurants = []
 
         if response.status_code != 200:
+            print("Error requesting data from Triposo API: ", response.status_code)
             return restaurants
 
         restaurant_data = response.json()['results']
--- capstone/views.py
+++ capstone/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from capstone.api_translation import *
+import string
 
+api_translator = Api_Translation()
 # Create your views here.
 class Home(View):
     def get(self, request):
         return render(request, "home.html")
 
     def post(self, request):
-        pass
+        location = request.POST.get("location", "")
+        top_cities_in_country = api_translator.top_cities_in_country(location)
+
+        # Get the best restaurants in each city and concatenate the city name to the restaurant to make it easy to
+        # distinguish in the HTML template. Also capitalize restaurant names because not all come capitalized
+        best_restaurants = []
+        for city in top_cities_in_country:
+            best_restaurants.append([city.replace("2C", ",").replace("_", " ") + "-" + string.capwords(restaurant) for
+                                     restaurant in api_translator.find_best_resturants(city)])
+
+        # Use list comprehension to replace 2C with a comma and _ with a space because API outputs some cities like:
+        # Niagara_Falls2C_Ontario rather than Niagara Falls, Ontario
+        cities = [city.replace("2C", ",").replace("_", " ") for city in top_cities_in_country]
+
+        # Flatten list of restaurants from [[City1-Rest1, City1-Rest2], [City2-Rest1, City2-Rest2]] to
+        # [City1-Rest1, City1-Rest2, City2-Rest1, City2-Rest2]
+        restaurants = [restaurant for sublist in best_restaurants for restaurant in sublist]
+
+        print(cities)
+        print(restaurants)
+
+        return render(request, "home.html", {'location': location, 'cities': cities, 'restaurants': restaurants})
 
 class About(View):
     def get(self, request):
