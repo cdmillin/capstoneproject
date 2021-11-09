d2a4870aab14507d57de05993671cdd1ddb4eb82
willb <wbarrera@uwm.edu>
2021-11-09 16:33:28 -0600

    Added navbar to city.html, search bar in the navbar now allows you to search for popular cities with a state and country, however, searching while you're in the about page current causes url issues and needs to be worked out later.

--- capstone/views.py
+++ capstone/views.py
@@ -50,6 +50,29 @@ class About(View):
     def get(self, request):
         return render(request, 'about.html')
 
+    #allows the search bar to function while on the about page.
+    def post(self, request):
+
+        if 'citybtn' in request.POST:
+            city = request.POST.get('citybtn', "")
+            city_formatted = city.replace(",", "2C").replace(" ", "_")
+
+            restaurants = {city: api_translator.find_best_resturants(city_formatted)}
+            hotels = {city: api_translator.find_best_hotels(city_formatted)}
+            sights = {city: api_translator.find_best_sightseeing(city_formatted)}
+
+            return render(request, "city.html", {'city': city, 'restaurants': restaurants, 'sights': sights, 'hotels': hotels})
+
+        elif 'location' in request.POST:
+            location = str(request.POST.get("location", "")).title()
+
+            top_cities_in_country = api_translator.top_cities_in_country(location)
+
+            cities = [city.replace("2C", ",").replace("_", " ") for city in top_cities_in_country]
+
+            return render(request, "home.html", {'location': location, 'cities': cities})
+
+
 class City(View):
     def get(self, request):
         return render(request, "city.html")
