c8c99aade2b6a735bf0feaa3f86ef95e6901f384
SiddharthPatel93 <patel93@uwm.edu>
2021-11-30 22:46:58 -0600

    sprint 4

--- capstone/views.py
+++ capstone/views.py
@@ -4,28 +4,28 @@ from capstone.api_translation import *
 import string
 
 api_translator = Api_Translation()
+
+
 # Create your views here.
 class Home(View):
     def get(self, request):
 
-        #location names are filled with string Destination as a placeholder.
-        #replace locationNames with popular destinations received from api.
-        #location info is also filled with a placeholder string.
+        # location names are filled with string Destination as a placeholder.
+        # replace locationNames with popular destinations received from api.
+        # location info is also filled with a placeholder string.
 
-        #location1name = "Destination 1"
-        #location1info = "Information about destination 1 and things to do"
+        # location1name = "Destination 1"
+        # location1info = "Information about destination 1 and things to do"
 
-        #location2name = "Destination 2"
-        #location2info = "Information about destination 2 and things to do"
+        # location2name = "Destination 2"
+        # location2info = "Information about destination 2 and things to do"
 
-        #location3name = "Destination 3"
-        #location3info = "Information about destination 3 and things to do."
+        # location3name = "Destination 3"
+        # location3info = "Information about destination 3 and things to do."
 
-        #return render(request, "home.html", {"location1": location1name, "location2": location2name, "location3": location3name, "location1info": location1info, "location2info": location2info, "location3info": location3info})
-
+        # return render(request, "home.html", {"location1": location1name, "location2": location2name, "location3": location3name, "location1info": location1info, "location2info": location2info, "location3info": location3info})
 
         return render(request, "home.html")
-
 
     def post(self, request):
         if 'citybtn' in request.POST:
@@ -36,7 +36,8 @@ class Home(View):
             hotels = {city: api_translator.find_best_hotels(city_formatted)}
             sights = {city: api_translator.find_best_sightseeing(city_formatted)}
 
-            return render(request, "city.html", {'city': city, 'restaurants': restaurants, 'sights': sights, 'hotels': hotels})
+            return render(request, "city.html",
+                          {'city': city, 'restaurants': restaurants, 'sights': sights, 'hotels': hotels})
 
         elif 'location' in request.POST:
             location = str(request.POST.get("location", "")).title()
@@ -45,13 +46,14 @@ class Home(View):
 
             cities = [city.replace("2C", ",").replace("_", " ") for city in top_cities_in_country]
 
-            return render(request, "home.html", {'location': location, 'cities': cities})
+            return render(request, "location.html", {'location': location, 'cities': cities})
+
 
 class About(View):
     def get(self, request):
         return render(request, 'about.html')
 
-    #allows the search bar to function while on the about page.
+    # allows the search bar to function while on the about page.
     def post(self, request):
 
         if 'citybtn' in request.POST:
@@ -62,16 +64,16 @@ class About(View):
             hotels = {city: api_translator.find_best_hotels(city_formatted)}
             sights = {city: api_translator.find_best_sightseeing(city_formatted)}
 
-            return render(request, "city.html", {'city': city, 'restaurants': restaurants, 'sights': sights, 'hotels': hotels})
+            return render(request, "city.html",
+                          {'city': city, 'restaurants': restaurants, 'sights': sights, 'hotels': hotels})
 
         elif 'location' in request.POST:
             location = str(request.POST.get("location", "")).title()
-
             top_cities_in_country = api_translator.top_cities_in_country(location)
 
             cities = [city.replace("2C", ",").replace("_", " ") for city in top_cities_in_country]
 
-            return render(request, "home.html", {'location': location, 'cities': cities})
+            return render(request, "location.html", {'location': location, 'cities': cities})
 
 
 class City(View):
@@ -86,3 +88,43 @@ class City(View):
         return render(request, "city.html", {'city': city})
 
 
+class Location(View):
+    def get(self, request):
+
+        # location names are filled with string Destination as a placeholder.
+        # replace locationNames with popular destinations received from api.
+        # location info is also filled with a placeholder string.
+
+        # location1name = "Destination 1"
+        # location1info = "Information about destination 1 and things to do"
+
+        # location2name = "Destination 2"
+        # location2info = "Information about destination 2 and things to do"
+
+        # location3name = "Destination 3"
+        # location3info = "Information about destination 3 and things to do."
+
+        # return render(request, "home.html", {"location1": location1name, "location2": location2name, "location3": location3name, "location1info": location1info, "location2info": location2info, "location3info": location3info})
+
+        return render(request, "location.html")
+
+    def post(self, request):
+        if 'citybtn' in request.POST:
+            city = request.POST.get('citybtn', "")
+            city_formatted = city.replace(",", "2C").replace(" ", "_")
+
+            restaurants = {city: api_translator.find_best_resturants(city_formatted)}
+            hotels = {city: api_translator.find_best_hotels(city_formatted)}
+            sights = {city: api_translator.find_best_sightseeing(city_formatted)}
+
+            return render(request, "city.html",
+                          {'city': city, 'restaurants': restaurants, 'sights': sights, 'hotels': hotels})
+
+        elif 'location' in request.POST:
+            location = str(request.POST.get("location", "")).title()
+
+            top_cities_in_country = api_translator.top_cities_in_country(location)
+
+            cities = [city.replace("2C", ",").replace("_", " ") for city in top_cities_in_country]
+
+            return render(request, "location.html", {'location': location, 'cities': cities})
